80c4f4bbb8c805e79dab7b20cffee2b5a44fea40
Georgi Ivanov <georgiivanovk@abv.bggit config --global user.email georgiivanovk@abv.bggit config user.email git config user.email georgiivanovk@abv.bg>
2022-05-28 16:44:04 +0300

    100/100

---
+++ Sets and Dictionaries Advanced - Exercises/05. Count Symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char ch = sentence[i];
+                if (!symbols.ContainsKey(ch))                
+                    symbols.Add(sentence[i], 1);        
+            
+                else               
+                    symbols[ch]++;
+
+
+            }
+           
+            foreach (var ch in symbols)
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+         
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Exercises/4. Even Times/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+  
+            for (int i = 0; i < count; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                
+                
+                if (!dic.ContainsKey(num))              
+                    dic.Add(num, 0);
+
+                dic[num]++; 
+            }
+            foreach (var num in dic)
+            {
+                if (num.Value % 2 == 0)                
+                    Console.WriteLine(num.Key);               
+            }
+            
+        }
+    }
+}
