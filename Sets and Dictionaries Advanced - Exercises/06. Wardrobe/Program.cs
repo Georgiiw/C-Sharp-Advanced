bb7978348de8d32317beb598552f89d1e3e8e26e
Georgi Ivanov <georgiivanovk@abv.bggit config --global user.email georgiivanovk@abv.bggit config user.email git config user.email georgiivanovk@abv.bg>
2022-05-28 17:11:16 +0300

    100/100

---
+++ Sets and Dictionaries Advanced - Exercises/06. Wardrobe/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ");              
+                string color = input[0];             
+                string[] clothes = input[1].Split(",");
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                for (int k = 0; k < clothes.Length; k++)
+                {
+                    if (!wardrobe[color].ContainsKey(clothes[k]))
+                    {
+                        wardrobe[color].Add(clothes[k], 1);
+
+                    }
+                    else
+                    {
+                        wardrobe[color][clothes[k]]++;
+                    }
+                }
+
+            }
+            string[] wantedCloth = Console.ReadLine().Split(" ");
+            foreach (var item in wardrobe)
+            {
+                Console.WriteLine($"{item.Key} clothes:");
+                var clothes = item.Value;
+                foreach (var cloth in clothes)
+                {
+                    if (cloth.Key == wantedCloth[1] && item.Key == wantedCloth[0])
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
