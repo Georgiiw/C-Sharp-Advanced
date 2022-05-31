129c318de1b495fae150b2c6af37199076649abf
Georgiiw <georgiivanovk@abv.bg>
2022-05-31 11:29:46 +0300

    100/100

---
+++ Functional-Programming-Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> func = n => n[0] == n.ToUpper()[0];
+            string[] text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(n => func(n))
+                .ToArray();
+
+            foreach (var word in text)           
+                Console.WriteLine(word);
+            
+        }
+    }
+}
