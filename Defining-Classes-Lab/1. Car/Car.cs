f1484bab39a48e51eae0fc87f6f8a551d0c383bb
Georgiiw <georgiivanovk@abv.bg>
2022-06-02 19:46:40 +0300

    100/100

---
+++ Defining-Classes-Lab/1. Car/Car.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+     class Car
+     {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * this.FuelConsumption;
+            if (consumption <= this.FuelQuantity)
+                this.FuelQuantity -= consumption;
+            else
+                Console.WriteLine("Not enough fuel to perform this trip!");
+        }
+
+        public string WhoAmI()
+        {
+            string info = $"Make:{this.Make} Model:{this.Model}" +
+                $" Year:{this.Year} Fuel:{this.FuelQuantity:f2} ";
+            return info;
+        }
+
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 0.8;
+
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+
+        }
+    }
+}
---
+++ Defining-Classes-Lab/1. Car/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+     public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            {
+                car.Make = "VW";
+                car.Model = "MK3";
+                car.Year = 1992;
+                car.FuelQuantity = 50;
+                car.FuelConsumption = 0.7;
+            }
+             
+            Console.WriteLine($"Make:{car.Make}; Model:{car.Model}; Year:{car.Year}.");
+            double distance = 45;
+            car.Drive(distance);
+            Console.WriteLine(car.WhoAmI());
+
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car carOne = new Car();
+            Car carTwo = new Car(make, model, year);
+            Car carThree = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+
+
+
+
+        }
+    }
+}
