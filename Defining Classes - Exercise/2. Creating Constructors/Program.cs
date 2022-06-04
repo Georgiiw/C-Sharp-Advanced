2e8d4525ec62b766654d0999578b64000f2d3430
Georgiiw <georgiivanovk@abv.bg>
2022-06-04 12:53:34 +0300

    100/100

---
+++ Defining Classes - Exercise/2. Creating Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+           
+            int age = int.Parse(Console.ReadLine());
+
+            Person person = new Person(age);
+            
+            string info = $"Name: {person.Name}, Age: {person.Age}";
+            
+            Console.WriteLine(info);
+           
+        }
+    }
+}
