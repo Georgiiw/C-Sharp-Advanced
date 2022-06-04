8a9c9db14c4641670133a172287d859d87f8e72a
Georgiiw <georgiivanovk@abv.bg>
2022-06-04 12:27:53 +0300

    100/100

---
+++ Defining Classes - Exercise/1. Define a Class Person/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person info = new Person(name, age);
+            string person = $"Name: {info.Name}, Age: {info.Age}";
+            Console.WriteLine(person);
+        }
+    }
+}
