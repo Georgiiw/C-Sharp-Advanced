40cc9e09a1e17ea4b2d85eb6d5c8813d0765eed3
Georgiiw <georgiivanovk@abv.bg>
2022-06-04 14:34:22 +0300

    100/100

---
+++ Defining Classes - Exercise/3. Oldest Family Member/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+     public class Family
+     {
+
+        public Family()
+        {
+            Members = new List<Person>();
+        }
+        private List<Person> Members; 
+
+        public void AddMember(Person member)
+        {
+           Members.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            Person person = Members
+                .OrderByDescending(x => x.Age)
+                .FirstOrDefault();
+            return person;
+        }
+
+    }
+}
---
+++ Defining Classes - Exercise/3. Oldest Family Member/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+     public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                string[] info = Console.ReadLine().Split(" ");
+                string name = info[0];
+                int age = int.Parse(info[1]);
+
+                Person member = new Person(name, age);
+
+                family.AddMember(member);
+            }
+            Person oldest = family.GetOldestMember();
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
