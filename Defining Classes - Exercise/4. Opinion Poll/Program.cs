5b39ad9698ea7588dbbc0e61769b6d2e7db48685
Georgiiw <georgiivanovk@abv.bg>
2022-06-05 11:33:34 +0300

    100/100*

---
+++ Defining Classes - Exercise/4. Opinion Poll/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split(" ");
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                Person person = new Person(name, age);
+
+                dict.Add(person.Name, person.Age);
+
+
+            }
+            foreach (var person in dict.OrderBy(x => x.Key))
+            {
+                if (person.Value >= 30)               
+                    Console.WriteLine($"{person.Key} - {person.Value}");
+                
+            }
+            
+           
+
+            
+        }
+    }
+}
