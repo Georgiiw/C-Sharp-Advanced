b1a5ccf93264478649f492b4dec1c8d1de88b1ab
Georgiiw <georgiivanovk@abv.bg>
2022-06-21 13:03:51 +0300

    50/100

---
+++ Exam prep/Stock Market/Investor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        private List<Stock> portfolio;
+        private string fullName;
+        private string emailAddress;
+        private decimal moneyToInvest;
+        private string brokerName;
+        public string FullName { get => fullName; set => fullName = value; }
+        public string EmailAddress { get => emailAddress; set => emailAddress = value; }
+        public decimal MoneyToInvest { get => moneyToInvest; set => moneyToInvest = value; }
+        public string BrokerName { get => brokerName; set => brokerName = value; }
+        public List<Stock> Portfolio { get => portfolio; set => portfolio = value; }
+
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            Portfolio = new List<Stock>();
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+        }
+         public int Count => this.Portfolio.Count;
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization >= 10000 && MoneyToInvest >= stock.MarketCapitalization)
+            {
+                this.Portfolio.Add(stock);
+                MoneyToInvest -= stock.MarketCapitalization;
+            }
+        }
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            
+            foreach(Stock stock in this.Portfolio)
+            {
+                if (stock.CompanyName == companyName && stock.MarketCapitalization == sellPrice)
+                {
+                    if (sellPrice <= stock.PricePerShare)
+                    {
+                        return $"Cannot sell {companyName}.";
+                    }
+                    else
+                    {
+                        this.Portfolio.Remove(stock);
+                        MoneyToInvest = stock.MarketCapitalization;
+                        return $"{companyName} was sold.";
+                    }
+                }
+
+            }
+
+            return $"{companyName} does not exist.";
+        }
+        public Stock FindStock(string companyName)
+        {
+            foreach (var stock in Portfolio)
+            {
+                if (stock.CompanyName == companyName)
+                {
+                    return stock;
+                }
+            }
+            return null;
+        }
+        public Stock FindBiggestCompany()
+        {
+            if (Portfolio.Any())
+            {
+                return this.Portfolio.Max();
+            }
+            return null;
+        }
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            Console.WriteLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var stock in Portfolio)
+            {
+
+                sb.Append(stock);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exam prep/Stock Market/Stocks.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        
+        private string companyName;
+        private string director;   
+        private decimal pricePerShare;  
+        private int totalNumberOfShares;
+        private decimal marketCapitalization;
+
+        public string CompanyName
+        {
+            get { return companyName; }
+            set { companyName = value; }
+        }
+        public string Director
+        {
+            get { return director; }
+            set { director = value; }
+        }
+        public decimal PricePerShare
+        {
+            get { return pricePerShare; }
+            set { pricePerShare = value; }
+        }
+        public int TotalNumberOfShares
+        {
+            get { return totalNumberOfShares; }
+            set { totalNumberOfShares = value; }
+        }
+        public decimal MarketCapitalization
+        {
+            get { return marketCapitalization; }
+            set { marketCapitalization = value; }
+        }
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            CompanyName = companyName;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumberOfShares;
+            MarketCapitalization = pricePerShare * totalNumberOfShares;
+           
+        }
+        
+        public override string ToString()
+        {
+            return $"Compnay: {CompanyName}" +
+                $"Director: {Director}" +
+                $"Price per share: ${PricePerShare}" +
+                $"Market capitalization: ${MarketCapitalization}";
+
+        }
+    }
+}
